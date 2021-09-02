ca465b3d16bf9f37ab21f10b52dafba20ec94a06
xapep <andraz.pavlic@edsolution.si>
2021-09-02 20:13:30 +0200

    dev

--- src/dca/tl_content.php
+++ src/dca/tl_content.php
@@ -10,3 +10,26 @@
  * @license   LGPL
  * @copyright 2015
  */
+
+/**
+ * Modify palettes
+*/
+$GLOBALS['TL_DCA']['tl_article']['palettes']['default'] = str_replace (
+  'customTpl',
+  'customTpl,containerCssClass',
+  $GLOBALS['TL_DCA']['tl_article']['palettes']['default']
+);
+
+$GLOBALS['TL_DCA']['tl_page']['fields'] += [
+  'containerCssClass' => [
+      'label' => &$GLOBALS['TL_LANG']['tl_article']['containerCssClass'],
+      'exclude' => true,
+      'search' => true,
+      'inputType' => 'text',
+      'eval' => [
+          'maxlength' => 255,
+          'tl_class' => 'w50',
+      ],
+      'sql' => "varchar(255) NOT NULL default ''",
+  ],
+];
