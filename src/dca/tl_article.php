2feb36ed35bf0329fd79daff15bff2b563325fe9
xapep <andraz.pavlic@edsolution.si>
2021-09-02 18:16:03 +0200

    structure

---
+++ src/dca/tl_article.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   ArticleContainerClass
+ * @author    EDsolution.si
+ * @license   LGPL
+ * @copyright 2015
+ */
---
+++ src/languages/en/default.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   ArticleContainerClass
+ * @author    EDsolution.si
+ * @license   LGPL
+ * @copyright 2015
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC'][''] = '';
